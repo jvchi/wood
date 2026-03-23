fcced45f296a728a84f2e55f29b5b24b2277aac1
jvchi <elechidestinyj@gmail.com>
2026-03-23 22:11:25 +0100

    now i understand how to get the max number in an array

--- pages/hero.jsx
+++ pages/hero.jsx
@@ -1,6 +1,17 @@
 
 
 export default function hero() {
+  
+  const array = [100, 101, 2, 4, 5, 6, 77, 200]
+  let max = 0
+
+  for (let i = 0; i < array.length; i++ ){
+    if(array[i] > max){
+      max = array[i]
+      
+    }
+  }
+  console.log(max)
   return (
     <main className='w-full h-screen flex justify-center items-center'>
       <h1 className='text-9xl'>wood</h1>
