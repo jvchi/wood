3bd737a2944cc0cadbd2d88dfdb53334a0748deb
jvchi <elechidestinyj@gmail.com>
2026-03-23 23:49:44 +0100

    got the 3d model set up, kinda

--- pages/hero.jsx
+++ pages/hero.jsx
@@ -1,20 +1,32 @@
-
-
+import { Canvas } from "@react-three/fiber"
+import Couch from "../models/Couch"
+import { OrbitControls, Bounds, Center } from "@react-three/drei"
 export default function hero() {
   
-  const array = [100, 101, 2, 4, 5, 6, 77, 200]
-  let max = 0
-
-  for (let i = 0; i < array.length; i++ ){
-    if(array[i] > max){
-      max = array[i]
-      
-    }
-  }
-  console.log(max)
   return (
-    <main className='w-full h-screen flex justify-center items-center'>
-      <h1 className='text-9xl'>wood</h1>
-    </main>
+
+    <section className="bg-neutral-200">
+      <span className="text-9xl ">
+        Couch
+      </span>
+      <Canvas 
+        camera={{position: [-50, 1.5, 50], fov: 1, near: 0.1}}
+        className="w-full min-h-screen flex justify-start items-center "
+        >
+          <ambientLight intensity={0.5}/>
+          <directionalLight position={[2,5,5]}/>
+          <axesHelper args={[5]} />
+          <OrbitControls/>
+          
+          <Bounds fit clip observe margin={0.4}>
+            <Center>
+              <Couch className='h-full w-full'/>
+            </Center>
+            
+          </Bounds>
+      </Canvas>
+    </section>
+    
+    
   )
 }
