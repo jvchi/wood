eb63bbb6ec319a8216207fc7594ede6c9c3a7d9d
jvchi <elechidestinyj@gmail.com>
2026-03-24 19:58:27 +0100

    added a hover animation (kinda) and it sucks

--- pages/hero.jsx
+++ pages/hero.jsx
@@ -1,11 +1,32 @@
-import { Canvas } from "@react-three/fiber"
 import Room from "../components/Room"
 import { OrbitControls, Bounds, Center } from "@react-three/drei"
 import { useControls } from "leva"
-import { useThree } from "@react-three/fiber"
-import { useEffect } from "react"
+import { useThree, useFrame, Canvas } from "@react-three/fiber"
+import { useEffect, useState, useRef } from "react"
 
+
+function CameraMouseMove({mouse}){
+  const {camera} = useThree();
+
+  const initial = useRef({
+    x: camera.position.x,
+    y: camera.position.y,
+    z: camera.position.z,
+  })
+
+  useFrame(()=>{
+    const targetX = initial.current.x + mouse.current.x * 0.5
+    const targetY = initial.current.y + mouse.current.y * 0.2
+
+    camera.position.x += (targetX - camera.position.x )* 0.02
+    camera.position.y += (targetY - camera.position.y)* 0.02
+
+    camera.lookAt(0,0,0)
+  })
+  return null
+}
 export default function Hero() {
+
   const {x, y, z, fov} = useControls('Camera',{
     x: {value: 7, min: -50, max: 50, step: 1},
     y: {value: 0, min: -20, max: 20, step: 1},
@@ -22,18 +43,34 @@ export default function Hero() {
     }, [x, y, z, fov, camera])
     return null
   }
+
+  const mouse = useRef({x: 0, y: 0});
+  function handleMouseMove(e){
+    mouse.current.x =( e.clientX / window.innerWidth - 0.5 ) * 2 
+    mouse.current.y = (e.clientY / window.innerHeight - 0.5) * 2
+    console.log(mouse.current.x, mouse.current.y)
+  }
+
+  function handleMouseLeave(e){
+    mouse.current = {x: 0, y: 0}
+  }
   return (
-    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+    <div 
+    onMouseMove={handleMouseMove}
+    onMouseLeave={handleMouseLeave}
+    style={{ position: "relative", width: "100vw", height: "100vh" }} 
+    className="overscroll-none">
 
       <Canvas
-        onPointerMove={()=> console.log('pointer')}
         style={{ position: "absolute", inset: 0 }}
         camera={{ position: [x, y, z], fov: 50 }}
       >
         <ambientLight intensity={0.5} />
         <directionalLight position={[2, 5, 5]} />
-        <OrbitControls />
+        <OrbitControls enableZoom={false} enablePan={false} />
+        {/* <Animated/> */}
         <CameraRig x={x} y={y} z={z} fov={fov}/>
+        <CameraMouseMove mouse={mouse}/>
         <Bounds clip observe margin={1}>
           <Center>
             <Room/>
@@ -45,7 +82,7 @@ export default function Hero() {
         style={{ position: "absolute", top: 32, left: 32, zIndex: 10 }}
         className="text-7xl sm:text-9xl"
       >
-        Couch
+        Couch <br/> <span className="mx-50">Place</span>
       </span>
 
     </div>
